ec358333b60ab367d1acc1c93878b606e802f231
Fazlur Rahman Khan <fyndfrk@gmail.com>
2022-10-12 13:57:11 +0530

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+sudo apt-get remove docker docker-engine docker.io containerd runc
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe br_netfilter
+modprobe overlay
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+apt-get -y update && apt-get install -y containerd.io
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+
 
 sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
 systemctl daemon-reload
