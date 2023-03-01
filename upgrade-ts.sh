4f40f2c9ce82281e0789784d43a02ed39101cec3
Fazlur Rahman Khan <fyndfrk@gmail.com>
2023-03-01 05:50:35 +0530

    Update upgrade-ts.sh

--- upgrade-ts.sh
+++ upgrade-ts.sh
@@ -6,7 +6,7 @@ sudo mkdir -p ~/backup
 sudo cp -p ~/.kube/config ~/backup/  >> /dev/null
 sudo sed -e 's/6443/9663/g' -i ~/.kube/config
 sudo cp -p /etc/kubernetes/manifests/kube-apiserver.yaml ~/backup/ >> /dev/null
-sudo sed -e 's/RBAC/RBAC,ROHAN/g' -i /etc/kubernetes/manifests/kube-apiserver.yaml
+sudo sed -e 's/RBAC/RBAC,FAZLUR/g' -i /etc/kubernetes/manifests/kube-apiserver.yaml
 sudo systemctl disable --now kubelet &> /dev/null
 sudo echo "Executing steps to upgrade your cluster... Please standby... "
 sudo sleep 10
